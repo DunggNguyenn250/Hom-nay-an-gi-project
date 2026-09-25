7764321b80ab8e3ad72a4e364eddbc67c55fe73a
DunggNguyenn <nguyenbadung2005@gmail.com>
2026-09-25 15:55:25 +0700

    Đổi tên application

--- BE/src/main/java/org/example/esport/HomnayangiApplication.java
+++ BE/src/main/java/org/example/esport/HomnayangiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EsportApplication {
+public class HomnayangiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EsportApplication.class, args);
+        SpringApplication.run(HomnayangiApplication.class, args);
     }
 
 }
